766f1c1af418d8ea443210f3759879df02218be8
Dejan Velimirović <dvsoftware@gmail.com>
2022-05-25 22:41:42 +0200

    require.go

---
+++ Sparkle.framework/require.go
@@ -0,0 +1,7 @@
+// Package sparkle contains required external dependencies from Sparkle.
+package sparkle
+
+import (
+	_ "github.com/DVSoftware/go-sparkle/Sparkle.framework/Headers"
+	_ "github.com/DVSoftware/go-sparkle/Sparkle.framework/PrivateHeaders"
+)
